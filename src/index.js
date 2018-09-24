b2bf73bb31230cbadefd2e168887daebff976bc8
devildummy <cyrok.13@gmail.com>
2018-09-24 22:55:59 +0300

    all works

--- src/index.js
+++ src/index.js
@@ -1,26 +1,62 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.toDo=[];
+    this.toDo.push(initialValue);
   }
 
   add(number) {
-    // your implementation
+    this.toDo.push('+',number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    this.toDo.push('-',number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    this.toDo.push('*',number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    this.toDo.push('/',number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    this.toDo.push('^',number);
+    return this;
+  }
+
+  toString(){
+    let arr=[];
+    this.toDo.forEach(function(item,i){arr[i]=item;});
+    for(let i=arr.length-1;i>0;i--){
+      if(arr[i]=='^'){
+        arr.splice(i-1,3,Math.pow(arr[i-1],arr[i+1]));
+        i++;
+      }
+    }
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]=='*' ){
+        arr.splice(i-1,3,arr[i-1]*arr[i+1]);
+        i--;
+      }else if(arr[i]=='/'){
+        arr.splice(i-1,3,arr[i-1]/arr[i+1]);
+        i--;
+      }
+    }
+    for(let i=0;i<arr.length;i++){
+      if(arr[i]=='+'){
+        arr.splice(i-1,3,arr[i-1]+arr[i+1]);
+        i--;
+      }else if(arr[i]=='-'){
+        arr.splice(i-1,3,arr[i-1]-arr[i+1]);
+        i--;
+      }
+    }
+  return arr[0];
   }
 }
 
